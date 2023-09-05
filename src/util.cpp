f8e75e1b700139698ef5a3376b00c4e120d8b5e4
Ben Patel <z5426928@ad.unsw.edu.au>
2023-09-05 16:56:13 +1200

    updated timing

--- src/util.cpp
+++ src/util.cpp
@@ -6,18 +6,15 @@ namespace TradingEngine::Util {
         struct std::tm* timeInfo = std::localtime(&currentTime);
         uint64_t h = (uint64_t)timeInfo->tm_hour;
         uint64_t m = (uint64_t)timeInfo->tm_min;
-
-        uint64_t s = (uint64_t)std::chrono::duration_cast<std::chrono::seconds>(
-        std::chrono::high_resolution_clock::now().time_since_epoch())
-        .count();
+        uint64_t s = (uint64_t)timeInfo->tm_sec;
 
         uint64_t ms = (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(
         std::chrono::high_resolution_clock::now().time_since_epoch())
-        .count();
+        .count() % 1000;
 
         uint64_t us = (uint64_t)std::chrono::duration_cast<std::chrono::microseconds>(
         std::chrono::high_resolution_clock::now().time_since_epoch())
-        .count();
+        .count() % 1000000;
 
         hours = h;
         minutes = m;
@@ -29,8 +26,7 @@ namespace TradingEngine::Util {
     std::string ExchangeTime::getString() {
         std::string h = (hours < 10) ? "0" + std::to_string(hours) : std::to_string(hours);
         std::string m = (minutes < 10) ? "0" + std::to_string(minutes) : std::to_string(minutes);
-        std::string s = (seconds < 100) ? "0" + std::to_string(seconds) : std::to_string(seconds);
-        s += (seconds < 10) ? "0" : "";
+        std::string s = (seconds < 10) ? "0" + std::to_string(seconds) : std::to_string(seconds);
 
         std::string str = h + ":" + m + ":"  + std::to_string(seconds) + ":"  + std::to_string(millis) + ":"  + std::to_string(micros);
         return str;
